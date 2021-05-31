41ea64191c6519087317a4f4b3e60a538ff299b3
andrescruz5193@gmail.com <andrescruz5193@gmail.com>
2021-05-30 20:28:31 -0500

    Se crea página de autenticación login

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get( '/',  ( req, res )=>{
     res.sendFile( path.resolve(__dirname, './views/index.html') );
 });
 
+app.get( '/login',  ( req, res )=>{
+    res.sendFile( path.resolve(__dirname, './views/login.html') );
+});
+
 app.listen( 3000, ()=>{
     console.log('Servidor escuchando por el puerto 3000');
 } );
