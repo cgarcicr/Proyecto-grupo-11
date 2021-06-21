55cdad0900ca8de858e6820c5160d44ead2e9e88
andrescruz5193@gmail.com <andrescruz5193@gmail.com>
2021-06-20 23:28:05 -0500

    Se crea las carpetas products, users y partials

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -5,7 +5,7 @@ const { request, response } = require('express');
 const path = require('path');
 
 let  obtenerLogin = ( req, res = response)=>{
-    res.sendFile( path.resolve(__dirname, '../views/login.html') );
+    res.sendFile( path.resolve(__dirname, '../views/users/login.html') );
 }
 
 module.exports = { obtenerLogin }
--- src/controllers/registroController.js
+++ src/controllers/registroController.js
@@ -5,7 +5,7 @@ const { request, response } = require('express');
 const path = require('path');
 
 let  obtenerRegistro = ( req, res = response)=>{
-    res.sendFile( path.resolve(__dirname, '../views/registro.html') );
+    res.sendFile( path.resolve(__dirname, '../views/users/registro.html') );
 }
 
 module.exports = { obtenerRegistro }
